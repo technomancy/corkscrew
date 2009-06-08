e80677e80ad5a1553fa3fb357c4f53a9affd2270
Phil Hagelberg <technomancy@gmail.com>
2009-06-07 22:22:13 -0700

    Make the sample easier to check.

--- sample/project.clj
+++ sample/project.clj
@@ -3,7 +3,8 @@
  :namespaces '(my-sample)
  :dependencies [["clojure" "1.0.0" "org.clojure"]
                 ;; group defaults to name
-                ["rome" "0.9"]]
+                ["rome" "0.9"]
+                ["tagsoup" "1.2" "org.ccil.cowan.tagsoup"]]
  :source-dependencies [["clojure-contrib" "r663" :svn
                         "http://clojure-contrib.googlecode.com/svn/trunk"]
                        ["enlive" "95b2558943f50bb9962fe7d500ede353f1b578f0"
--- sample/src/my/sample.clj
+++ sample/src/my/sample.clj
@@ -1,4 +1,8 @@
 (ns my.sample
+  (:use [clojure.contrib duck-streams]
+        [net.cgrand enlive-html])
+  (:import [org.jdom.input TextBuffer]
+           [com.sun.syndication.io SyndFeedInput])
   (:gen-class))
 
 (defn -main [& args]
